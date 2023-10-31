1a375802a9c271f4d4853683866bd06ff301b502
Sam Christopher <113563778+SamChrstopher@users.noreply.github.com>
2023-10-31 08:07:55 -0700

    Add files via upload

---
+++ 141. Linked List Cycle/141. Linked List Cycle.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    var slow=head
+    var fast=head
+    while(fast!==null && fast.next!==null){
+        fast=fast.next.next
+        if(fast===slow){
+            return true
+        }
+        slow=slow.next
+    }
+    return false    
+};
